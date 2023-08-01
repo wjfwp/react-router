ae39ecdb45b4c1460f31047631016a1b50786c43
wjfwp <sis6710@gmail.com>
2023-08-01 17:04:24 +0900

    자바랑 restAPI 연결

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-import NewsHome from "./component2/NewsHome";
+import { useEffect } from "react"
 
-import Header from './layout/Header';
 
 function App() {
 
-  return(
-   <Routes>
-    <Route element={ <Header/> }>
-      <Route path="/" element={ <NewsHome/> } />
-      <Route path="/:category" element={ <NewsHome/> } />
-    </Route>
-   </Routes>
+  useEffect(() => {
+    fetch('http://localhost:8181/api/v1/getInfo',{
+      method : "post",
+      headers : {"Content-Type" : "application/json"},
+      body : JSON.stringify({"num" : 1, "name" : "리액트에서 보낸 데이터"})
+    })
+    .then( response => response.json() )
+    .then( t => console.log(t) )
+  }, []);
+
+  return (
+    <div>
+      
+    </div>
   )
 }
 
